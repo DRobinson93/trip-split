58093611708797d185aeaa72060dbc37913ff12b
drobinson93 <danpaulrob1@gmail.com>
2020-07-02 12:53:25 -0400

    add data attrs to totalWithoutTip

--- src/views/Tip.vue
+++ src/views/Tip.vue
@@ -7,7 +7,8 @@
             Total $
           </label>
           <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                 id="totalWithoutTip" type="number" placeholder="Total $" v-model.number="totalWithoutTip">
+                 id="totalWithoutTip" pattern="\d*" type="number" step="0.01"
+                 placeholder="Total $" v-model.number="totalWithoutTip">
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="tipPercent">
